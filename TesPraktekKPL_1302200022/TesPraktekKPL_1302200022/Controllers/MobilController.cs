ec14e7c104d50708a15d6795f9fa18dea3c74dac
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2024-01-24 13:57:52 +0700

    menambahkan project tes praktik

---
+++ TesPraktekKPL_1302200022/TesPraktekKPL_1302200022/Controllers/MobilController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TesPraktekKPL_1302200022.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MobilController : ControllerBase
+    {
+        private static List<Mobil> MobilList = new List<Mobil>()
+        {
+            new Mobil("Toyota", "Prius", "Red"),
+            new Mobil("BMW", "i7", "White"),
+            new Mobil("Honda", "NSX", "Purple")
+        };
+        // GET: api/<MobilController>
+        [HttpGet]
+        public IEnumerable<Mobil> Get()
+        {
+            return MobilList;
+        }
+
+        // GET api/<MobilController>/5
+        [HttpGet("{id}")]
+        public Mobil Get(int id)
+        {
+            return MobilList[id];
+        }
+
+        // POST api/<MobilController>
+        [HttpPost]
+        public void Post([FromBody] Mobil value)
+        {
+            MobilList.Add(value);
+        }
+
+        // PUT api/<MobilController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MobilController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            MobilList.RemoveAt(id);
+        }
+    }
+}
---
+++ TesPraktekKPL_1302200022/TesPraktekKPL_1302200022/Mobil.cs
@@ -0,0 +1,16 @@
+﻿namespace TesPraktekKPL_1302200022
+{
+    public class Mobil
+    {
+        public string brand { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public Mobil(string brand, string name, string description)
+        {
+            this.brand = brand;
+            this.name = name;
+            this.description = description;
+        }
+    }
+}
